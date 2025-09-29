8a725e686e3fd161cb9e0db1c10c76102086c2c1
Jieun <jeeneep@naver.com>
2025-09-29 16:26:35 +0900

    Initial commit

---
+++ src/main/java/com/the11job/backend/Application.java
@@ -0,0 +1,13 @@
+package com.the11job.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/the11job/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the11job.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
